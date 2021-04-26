4b877fe02a82bf0a4a0e6ab4684a0c442950bf26
Gleb <rojomodmini4@mail.ru>
2021-04-26 22:43:27 +0600

    js project 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('main', 'second');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privet: false
+};
+
+const a = prompt('Один из последних просмотренных филмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных филмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
