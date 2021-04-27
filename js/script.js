b03a02121c698a71330ace4dd7def09ea2fa271f
Gleb <rojomodmini4@mail.ru>
2021-04-27 12:08:48 +0600

    if,for,while,do

--- js/script.js
+++ js/script.js
@@ -1,36 +1,23 @@
 "use strict";
 
-if (1) {
-    console.log('Ok!');
-} else {
-    console.log('Error');
+let num = 50;
+
+/* while (num <= 55) {
+    console.log(num);
+    num++;
+} */
+
+/* do {
+    console.log(num);
+    num++;
 }
+while (num < 55); */
 
+for (let i = 1; i < 10; i++) {
+    if (i == 6){
+        /* break; */
+        continue;
+    }
 
-
-/* if(num < 49) {
-    console.log('Error');
-} else if (num > 100) {
-    console.log('Много');
-} else {
-    console.log('ok!');
-}
-
-(num === 50) ? console.log('ok!') : console.log('Error'); */
-
-const num = 50;
-
-switch (num) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-    console.log('Неверно');
-        break;
-    case 50:
-        console.log('Верно');
-        break;
-    default:
-        console.log('Не получилось');
-        break;
+    console.log(i);
 }
