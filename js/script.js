6ee698bee2fcf41f2cceab6e65cb32d583aecdd1
Gleb <rojomodmini4@mail.ru>
2021-04-30 23:35:21 +0600

    callback

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
- "use strict";
+  "use strict";
 
-let numberOfFilms;
+  function first() {
+      // Do something
+      setTimeout(function () {
+          console.log(1);
+      }, 500);
+  }
+
+  function second() {
+      console.log(2);
+  }
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+function done() {
+    console.log('Я прошел этот урок!');
+}
+
+learnJS('JavaScript', done);
+
+
+
+/* let numberOfFilms;
 
 function start() {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
@@ -35,7 +61,7 @@ function rememberMyFilms() {
     }
 }
 
-/* rememberMyFilms (); */
+ rememberMyFilms (); 
 
 function detectPersonalLevel() {
     if (personalMovieDB.count < 10) {
@@ -49,7 +75,7 @@ function detectPersonalLevel() {
     }
 }
 
-/* detectPersonalLevel(); */
+ detectPersonalLevel();
 
 function showMyDB (hidden) {
     if (!hidden) {
@@ -66,4 +92,4 @@ function writeYourGenres() {
     }
 }
 
-writeYourGenres();
+writeYourGenres(); */
