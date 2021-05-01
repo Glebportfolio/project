4b863d76b77a52e7f362a02a2e6746261b0d0aac
Gleb <rojomodmini4@mail.ru>
2021-05-02 00:40:06 +0600

    massive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,85 @@
-  "use strict";
+"use strict";
 
-  function first() {
+ const arr = [2, 13, 26, 8, 10];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+} 
+
+
+arr[99] = 0;
+console.log(arr.length);
+console.log(arr);
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+
+ arr.pop();
+arr.push(10);
+
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+}
+
+ for (let value of arr) {
+    console.log(value);
+}
+
+const str = prompt('','');
+const products = str.split('','');
+products.sort();
+console.log(products.join('; '));
+
+
+/*
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+ */
+/* console.log(Object.keys(options).length);
+ */
+/* console.log(options.name);
+
+delete options.name;
+
+console.log(options); */
+
+/* let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
+console.log(counter);
+ */
+
+
+/*   function first() {
       // Do something
       setTimeout(function () {
           console.log(1);
@@ -22,8 +101,7 @@ function done() {
     console.log('Я прошел этот урок!');
 }
 
-learnJS('JavaScript', done);
-
+learnJS('JavaScript', done); */
 
 
 /* let numberOfFilms;
@@ -93,3 +171,5 @@ function writeYourGenres() {
 }
 
 writeYourGenres(); */
+
+
