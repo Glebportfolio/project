ca4d1c12ba7a2832495b85fcd87f9a9d62c2f07f
Gleb <rojomodmini4@mail.ru>
2021-05-18 20:27:17 +0600

    test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
  "use strict";
 
-// to string
+// let x = 5; alert( x++);
+
+// [ ] + false - null + true
+// console.log([] + false - null + true); // "false"
+
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+//console.log([] + 1 + 2); // '12'
+
+// alert( '1'[0] );
+
+// console.log(2 && 1 && null && 0 && undefined);
+// И запинается на лжи
+// ИЛИ запинается на правде
+
+// console.log(!! (1 && 2) === (1 && 2));
+           // 3
+//alert(null || 2 && 3 || 4);
+
+// const a = [1, 2, 3]; 
+// const b = [1, 2, 3];
+
+// console.log(a == b);
+
+//alert(+'infinity');
+
+// console.log('Ежик' > 'яблоко');
+
+console.log(0 || '' || 2 || undefined) || true || false;
+
+
+/* // to string
 
 // 1)
 console.log(typeof(String(null)));
@@ -50,7 +83,7 @@ if (switcher) {
 console.log(typeof(Boolean('4')));
 
 // 3)
-console.log(typeof(!!'4444'));
+console.log(typeof(!!'4444')); */
 
 
 /*  function hello() {
