39e201a8067beb7acb0164e33ef0a71b133a3063
Gleb <rojomodmini4@mail.ru>
2021-05-24 17:28:42 +0600

    events and their handlers

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+/* 'use strict';
 
 const movieDB = {
     movies: [
@@ -48,7 +48,45 @@ movieDB.movies.forEach((film, i) => {
         <div class="delete"></div>
     </li>
     `;
-}); 
+});  */
+
+
+const btns = document.querySelector('button'),
+      overlay = document.querySelector('.overlay');
+/* btn.onclick = function() {
+    alert('click');
+};
+
+btn.onclick = function() {
+    alert('Second click');
+};
+ */
+
+// let i = 0
+const deleteElement = (e) => {
+    console.log(e.target);
+    console.log(e.type);
+/*     i++;
+    if (i ==1) {
+        btn.removeEventListener('click', deleteElement);
+    } */
+};
+
+/* btn.addEventListener('click', deleteElement);
+overlay.addEventListener('click', deleteElement); */
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function(event){
+    event.preventDefault();
+
+    console.log(event.target);
+});
+
 
 
 
