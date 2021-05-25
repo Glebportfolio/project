e23b6ee850083e98e9f16c1f83eef39b9ce20148
Gleb <rojomodmini4@mail.ru>
2021-05-25 19:15:04 +0600

    defer and async

--- js/script.js
+++ js/script.js
@@ -1,33 +1,16 @@
-// touchstart
-// touchmove
-// touchend
-// touchenter
-// touchleave
-// touchcancel
+"user strict";
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
+const p = document.querySelectorAll('p');
+console.log(p);
 
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
 
-        console.log('Start');
-        console.log(e.targetTouches);
-    });
 
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = "src";
+    script.async = false;
+    document.body.append(script);
+}
 
-        console.log(e.targetTouches[0].pageX);
-    });
-
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
-
-        console.log('End');
-    });
-});
-
-// touches
-// targetTouches
-// changedTouches
+loadScript("js/test.js");
+loadScript("js/some.js");
