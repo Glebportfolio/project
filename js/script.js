729102a654cca1b3a7552b762efde4356ebd0e71
Gleb <rojomodmini4@mail.ru>
2021-06-01 18:46:17 +0600

    practic 9

--- js/script.js
+++ js/script.js
@@ -99,12 +99,15 @@ window.addEventListener('DOMContentLoaded', function() {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
 
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTImerId);
+        }
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -126,4 +129,15 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }
     });
+
+    const modalTImerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageXOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
